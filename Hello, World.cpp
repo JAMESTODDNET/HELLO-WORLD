f27be76cb44e8e1f2c598424523e1a2adf8498bd
JAMESTODDNET <36094834+JAMESTODDNET@users.noreply.github.com>
2018-02-02 22:34:02 -0600

    Add files via upload

---
+++ Hello, World.cpp
@@ -0,0 +1,16 @@
+// Hello, World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    int pause;
+	cout << "Hello, World" << endl;
+	cin >> pause;
+	return 0;
+}
+
